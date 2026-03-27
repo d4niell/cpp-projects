bbad5f15b7e577c0ad5b2187d73fe436c6df0e54
d4niel1337 <gr315743@gradia.fi>
2026-03-27 10:36:08 +0200

    Added blazor App Project

---
+++ BlazorApp1/Todoitem.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SqlTypes;
+
+namespace BlazorApp1
+{
+    public class Todoitem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
